1a7d021bd5d74bda57d0f07baaab5f2dd2403fd1
sho-ta24 <ushota201122@gmail.com>
2021-04-10 19:22:47 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,7 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    @wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -31,12 +31,21 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
+      weeks = @todays_date.wday + x
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
+        
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = weeks# wdayメソッドを用いて取得した数値
+      if wday_num >= 7#「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :week =>(@wdays[wday_num])}
       @week_days.push(days)
     end
 
   end
 end
+#  %w: 曜日を表す数。日曜日が0(0-6)
